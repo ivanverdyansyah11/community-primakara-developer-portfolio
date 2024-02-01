bfeeb68d7c411a4b54790bc1bd775b8fff526692
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-02 05:32:46 +0800

    Progress: Finish & Responsive Section About

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -146,6 +146,7 @@
     </section>
     {{-- END SECTION EVENT --}}
 
+    {{-- SECTION BENEFIT --}}
     <section class="benefit section-gap">
         <div class="row justify-content-between align-items-end">
             <div class="col-lg-7 mb-3 mb-lg-0">
@@ -194,26 +195,115 @@
             </div>
         </div>
     </section>
+    {{-- END SECTION BENEFIT --}}
+
+    {{-- SECTION ABOUT --}}
+    <section class="about section-gap">
+        <div class="row justify-content-between align-items-center">
+            <div class="col-lg-6">
+                <h2 class="title">Empowering Innovators, Connecting Creators: The Primakara Developer Community Story</h2>
+                <div class="scroll-button">
+                    <div class="button-group d-flex">
+                        <button type="button" class="button-small button-history active">
+                            <span>Definition & History</span>
+                        </button>
+                        <button type="button" class="button-small button-vission">
+                            <span>Vission & Mission</span>
+                        </button>
+                        <button type="button" class="button-small button-goal">
+                            <span>Purpose & Goals</span>
+                        </button>
+                    </div>
+                </div>
+                <div class="paragraph-group paragraph-about">
+                    <p class="paragraph-small paragraph-one">At Primakara Developer Community, we believe in the transformative power of collaboration and code. Our community is a vibrant hub where developers, creators, and innovators converge to share knowledge, and build projects.</p>
+                    <p class="paragraph-small paragraph-two">Discover the essence of Primakara, where passion meets proficiency, and where every line of code contributes to a collective journey of digital excellence.</p>
+                </div>
+                <div class="fact-wrapper">
+                    <div class="row row-cols-2 row-cols-md-4 justify-content-between align-items-center">
+                        <div class="col fact-item">
+                            <h1 class="item-value">361+</h1>
+                            <p class="item-caption">Total Followings</p>
+                        </div>
+                        <div class="col fact-item">
+                            <h1 class="item-value">15+</h1>
+                            <p class="item-caption">Total Mentors</p>
+                        </div>
+                        <div class="col fact-item mt-4 mt-md-0">
+                            <h1 class="item-value">03+</h1>
+                            <p class="item-caption">Total Class Learning</p>
+                        </div>
+                        <div class="col fact-item mt-4 mt-md-0">
+                            <h1 class="item-value">24+</h1>
+                            <p class="item-caption">Total Organized</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-xl-5 d-none d-lg-inline-block">
+                <img src="{{ asset('assets/images/about/banner-about.svg') }}" alt="About Banner Image" class="img-fluid" style="width: 100%;">
+            </div>
+        </div>
+    </section>
+    {{-- END SECTION ABOUT --}}
 
     @include('modals.class')
     @include('modals.benefit')
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <script>
-        const swiper = new Swiper(".swiperEvent", {
-            slidesPerView: 1,
-            spaceBetween: 20,
-            loop: true,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-            },
-            breakpoints: {
-                768: {
-                    slidesPerView: 2,
-                    spaceBetween: 20,
+    @push('script')
+        <script>
+            const buttonSmall = document.querySelectorAll('.button-small');
+            const buttonHistory = document.querySelector('.button-history');
+            const buttonVission = document.querySelector('.button-vission');
+            const buttonGoal = document.querySelector('.button-goal');
+
+            const paragraphAbout = document.querySelector('.paragraph-about');
+            const paragraphOne = document.querySelector('.paragraph-one');
+            const paragraphTwo = document.querySelector('.paragraph-two');
+
+            buttonSmall.forEach(button => {
+                button.addEventListener('click', function() {
+                    if (!button.classList.contains('active')) {
+                        const buttonActive = document.querySelector('.button-small.active');
+                        buttonActive.classList.remove('active');
+
+                        button.classList.add('active');
+                        paragraphAbout.classList.add('hide');
+
+                        setTimeout(() => {
+                            if (button.classList.contains('button-history')) {
+                                paragraphOne.innerHTML = 'At Primakara Developer Community, we believe in the transformative power of collaboration and code. Our community is a vibrant hub where developers, creators, and innovators converge to share knowledge, and build projects.';
+                                paragraphTwo.innerHTML = 'Discover the essence of Primakara, where passion meets proficiency, and where every line of code contributes to a collective journey of digital excellence.';
+                            } else if (button.classList.contains('button-vission')) {
+                                paragraphOne.innerHTML = 'At Primakara Developer, our vision is to empower a global community of developers, fostering a collaborative environment that drives innovation and excellence in technology.';
+                                paragraphTwo.innerHTML = 'Our mission is to provide a dynamic platform where developers can learn, connect, and contribute to impactful projects. By nurturing a culture of continuous learning and collaboration, we strive to be a catalyst for positive change in the digital landscape. Join us as we collectively shape the future of technology.';
+                            } else if (button.classList.contains('button-goal')) {
+                                paragraphOne.innerHTML = 'At Primakara Developer, our purpose is to create a vibrant and inclusive space where developers thrive in innovation and collaboration. We envision a community-driven by a shared passion for technology and a commitment to continuous learning.';
+                                paragraphTwo.innerHTML = 'Our goals include fostering a culture of knowledge-sharing, providing valuable resources, and building a supportive environment where every developer can achieve their full potential. Join us in our journey to advance skills, create meaningful connections, and contribute to the ever-evolving world of technology.';
+                            }
+                            
+                            paragraphAbout.classList.remove('hide');
+                        }, '500');
+                    }
+                })
+            });
+
+            const swiper = new Swiper(".swiperEvent", {
+                slidesPerView: 1,
+                spaceBetween: 20,
+                loop: true,
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
                 },
-            },
-        });
-    </script>
+                breakpoints: {
+                    768: {
+                        slidesPerView: 2,
+                        spaceBetween: 20,
+                    },
+                },
+            });
+        </script>
+    @endpush
 @endsection
