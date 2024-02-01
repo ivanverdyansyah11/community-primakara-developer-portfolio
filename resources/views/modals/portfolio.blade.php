cc479c1b3fa99e9ad49e9415e4a9e07520096392
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-02 06:19:43 +0800

    Progress: Finish Make Modal Portfolio

---
+++ resources/views/modals/portfolio.blade.php
@@ -0,0 +1,247 @@
+<div class="modal fade" id="portfolioOneModal" tabindex="-1" aria-labelledby="portfolioOneModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content content-portfolio">
+            <div class="header-wrapper d-flex align-items-center justify-content-between">
+                <h4 class="modal-headline">Landing Page Speaker</h4>
+                <button type="button" data-bs-dismiss="modal" style="all: unset; cursor: pointer;">
+                    <img src="{{ asset('assets/images/icons/exit.svg') }}" alt="Exit Modal Icon" class="img-fluid" width="18">
+                </button>
+            </div>
+            <img src="{{ asset('assets/images/portfolios/portfolio-1.webp') }}" alt="Portfolio Image" class="img-fluid portfolio-image" style="width: 100%;">
+            <p class="modal-description">Introducing the Speaker Landing Page, where impactful messages find their voice. Our meticulously designed and user-friendly landing page for speakers is a gateway to communicate your expertise, showcase previous talks, and engage with your audience. Amplify your influence with a dynamic online presence that captivates and resonates.</p>
+            <div class="technology-wrapper d-flex flex-wrap">
+                <div class="technology-item">HTML</div>
+                <div class="technology-item">CSS 3</div>
+                <div class="technology-item">Java Script</div>
+                <div class="technology-item">PHP</div>
+                <div class="technology-item">Laravel</div>
+                <div class="technology-item">Swiper</div>
+            </div>
+            <div class="button-group d-flex">
+                <a href="#" class="button-modal">
+                    See Github
+                </a>
+                <a href="#" class="button-modal">
+                    See Preview
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="portfolioTwoModal" tabindex="-1" aria-labelledby="portfolioTwoModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content content-portfolio">
+            <div class="header-wrapper d-flex align-items-center justify-content-between">
+                <h4 class="modal-headline">Landing Page Studio</h4>
+                <button type="button" data-bs-dismiss="modal" style="all: unset; cursor: pointer;">
+                    <img src="{{ asset('assets/images/icons/exit.svg') }}" alt="Exit Modal Icon" class="img-fluid" width="18">
+                </button>
+            </div>
+            <img src="{{ asset('assets/images/portfolios/portfolio-2.webp') }}" alt="Portfolio Image" class="img-fluid portfolio-image" style="width: 100%;">
+            <p class="modal-description">Step into the Studio Landing Page, a virtual canvas for creative minds. This landing page is meticulously crafted for studios seeking an online showcase. Display your portfolio, highlight past projects, and provide a glimpse into your creative process. Elevate your studio's online identity with a visually stunning and functional space.</p>
+            <div class="technology-wrapper d-flex flex-wrap">
+                <div class="technology-item">HTML</div>
+                <div class="technology-item">CSS 3</div>
+                <div class="technology-item">Java Script</div>
+                <div class="technology-item">PHP</div>
+                <div class="technology-item">Laravel</div>
+                <div class="technology-item">Swiper</div>
+            </div>
+            <div class="button-group d-flex">
+                <a href="#" class="button-modal">
+                    See Github
+                </a>
+                <a href="#" class="button-modal">
+                    See Preview
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="portfolioThreeModal" tabindex="-1" aria-labelledby="portfolioThreeModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content content-portfolio">
+            <div class="header-wrapper d-flex align-items-center justify-content-between">
+                <h4 class="modal-headline">Landing Page Planner</h4>
+                <button type="button" data-bs-dismiss="modal" style="all: unset; cursor: pointer;">
+                    <img src="{{ asset('assets/images/icons/exit.svg') }}" alt="Exit Modal Icon" class="img-fluid" width="18">
+                </button>
+            </div>
+            <img src="{{ asset('assets/images/portfolios/portfolio-3.webp') }}" alt="Portfolio Image" class="img-fluid portfolio-image" style="width: 100%;">
+            <p class="modal-description">Welcome to the Planner Landing Page – your digital organizer and event planner's paradise. Seamlessly present your planning services, highlight successful events, and engage potential clients effortlessly. With an intuitive design, this landing page is your ally in conveying the essence of meticulous event planning.</p>
+            <div class="technology-wrapper d-flex flex-wrap">
+                <div class="technology-item">HTML</div>
+                <div class="technology-item">CSS 3</div>
+                <div class="technology-item">Java Script</div>
+                <div class="technology-item">PHP</div>
+                <div class="technology-item">Laravel</div>
+                <div class="technology-item">Swiper</div>
+            </div>
+            <div class="button-group d-flex">
+                <a href="#" class="button-modal">
+                    See Github
+                </a>
+                <a href="#" class="button-modal">
+                    See Preview
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="portfolioFourModal" tabindex="-1" aria-labelledby="portfolioFourModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content content-portfolio">
+            <div class="header-wrapper d-flex align-items-center justify-content-between">
+                <h4 class="modal-headline">Personal Portfolio SPA</h4>
+                <button type="button" data-bs-dismiss="modal" style="all: unset; cursor: pointer;">
+                    <img src="{{ asset('assets/images/icons/exit.svg') }}" alt="Exit Modal Icon" class="img-fluid" width="18">
+                </button>
+            </div>
+            <img src="{{ asset('assets/images/portfolios/portfolio-4.webp') }}" alt="Portfolio Image" class="img-fluid portfolio-image" style="width: 100%;">
+            <p class="modal-description">Experience the Personal Portfolio SPA, a serene space to showcase your skills and accomplishments. Crafted for professionals in the tech and creative industries, this Single Page Application (SPA) offers an immersive journey through your projects, skills, and experiences. Elevate your personal brand with an elegant and interactive portfolio.</p>
+            <div class="technology-wrapper d-flex flex-wrap">
+                <div class="technology-item">HTML</div>
+                <div class="technology-item">CSS 3</div>
+                <div class="technology-item">Java Script</div>
+                <div class="technology-item">PHP</div>
+                <div class="technology-item">Laravel</div>
+                <div class="technology-item">Swiper</div>
+            </div>
+            <div class="button-group d-flex">
+                <a href="#" class="button-modal">
+                    See Github
+                </a>
+                <a href="#" class="button-modal">
+                    See Preview
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="portfolioFiveModal" tabindex="-1" aria-labelledby="portfolioFiveModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content content-portfolio">
+            <div class="header-wrapper d-flex align-items-center justify-content-between">
+                <h4 class="modal-headline">Landing Page SaaS</h4>
+                <button type="button" data-bs-dismiss="modal" style="all: unset; cursor: pointer;">
+                    <img src="{{ asset('assets/images/icons/exit.svg') }}" alt="Exit Modal Icon" class="img-fluid" width="18">
+                </button>
+            </div>
+            <img src="{{ asset('assets/images/portfolios/portfolio-5.webp') }}" alt="Portfolio Image" class="img-fluid portfolio-image" style="width: 100%;">
+            <p class="modal-description">Enter the world of innovation with the SaaS Landing Page – your gateway to software excellence. Tailored for Software as a Service (SaaS) providers, this landing page elegantly presents your product features, benefits, and customer testimonials. Capture the essence of your software solutions and engage your target audience effortlessly.</p>
+            <div class="technology-wrapper d-flex flex-wrap">
+                <div class="technology-item">HTML</div>
+                <div class="technology-item">CSS 3</div>
+                <div class="technology-item">Java Script</div>
+                <div class="technology-item">PHP</div>
+                <div class="technology-item">Laravel</div>
+                <div class="technology-item">Swiper</div>
+            </div>
+            <div class="button-group d-flex">
+                <a href="#" class="button-modal">
+                    See Github
+                </a>
+                <a href="#" class="button-modal">
+                    See Preview
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="portfolioSixModal" tabindex="-1" aria-labelledby="portfolioSixModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content content-portfolio">
+            <div class="header-wrapper d-flex align-items-center justify-content-between">
+                <h4 class="modal-headline">Landing Page Hiking</h4>
+                <button type="button" data-bs-dismiss="modal" style="all: unset; cursor: pointer;">
+                    <img src="{{ asset('assets/images/icons/exit.svg') }}" alt="Exit Modal Icon" class="img-fluid" width="18">
+                </button>
+            </div>
+            <img src="{{ asset('assets/images/portfolios/portfolio-6.webp') }}" alt="Portfolio Image" class="img-fluid portfolio-image" style="width: 100%;">
+            <p class="modal-description">Embark on an adventure with the Hiking Landing Page – where the great outdoors meet digital exploration. This landing page is designed for hiking and adventure enthusiasts, providing a visual journey through scenic trails, expedition details, and upcoming events. Inspire fellow adventurers and share the thrill of the trail.</p>
+            <div class="technology-wrapper d-flex flex-wrap">
+                <div class="technology-item">HTML</div>
+                <div class="technology-item">CSS 3</div>
+                <div class="technology-item">Java Script</div>
+                <div class="technology-item">PHP</div>
+                <div class="technology-item">Laravel</div>
+                <div class="technology-item">Swiper</div>
+            </div>
+            <div class="button-group d-flex">
+                <a href="#" class="button-modal">
+                    See Github
+                </a>
+                <a href="#" class="button-modal">
+                    See Preview
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="portfolioSevenModal" tabindex="-1" aria-labelledby="portfolioSevenModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content content-portfolio">
+            <div class="header-wrapper d-flex align-items-center justify-content-between">
+                <h4 class="modal-headline">Landing Page Heaven</h4>
+                <button type="button" data-bs-dismiss="modal" style="all: unset; cursor: pointer;">
+                    <img src="{{ asset('assets/images/icons/exit.svg') }}" alt="Exit Modal Icon" class="img-fluid" width="18">
+                </button>
+            </div>
+            <img src="{{ asset('assets/images/portfolios/portfolio-7.webp') }}" alt="Portfolio Image" class="img-fluid portfolio-image" style="width: 100%;">
+            <p class="modal-description">Discover a slice of digital paradise with the Heaven Landing Page. Tailored for spiritual or wellness services, this landing page offers a tranquil online sanctuary. Share teachings, promote events, and foster a sense of community with a serene and inviting design. Immerse visitors in a digital haven that aligns with your spiritual vision.</p>
+            <div class="technology-wrapper d-flex flex-wrap">
+                <div class="technology-item">HTML</div>
+                <div class="technology-item">CSS 3</div>
+                <div class="technology-item">Java Script</div>
+                <div class="technology-item">PHP</div>
+                <div class="technology-item">Laravel</div>
+                <div class="technology-item">Swiper</div>
+            </div>
+            <div class="button-group d-flex">
+                <a href="#" class="button-modal">
+                    See Github
+                </a>
+                <a href="#" class="button-modal">
+                    See Preview
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="portfolioEightModal" tabindex="-1" aria-labelledby="portfolioEightModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content content-portfolio">
+            <div class="header-wrapper d-flex align-items-center justify-content-between">
+                <h4 class="modal-headline">Landing Page Restaurant</h4>
+                <button type="button" data-bs-dismiss="modal" style="all: unset; cursor: pointer;">
+                    <img src="{{ asset('assets/images/icons/exit.svg') }}" alt="Exit Modal Icon" class="img-fluid" width="18">
+                </button>
+            </div>
+            <img src="{{ asset('assets/images/portfolios/portfolio-8.webp') }}" alt="Portfolio Image" class="img-fluid portfolio-image" style="width: 100%;">
+            <p class="modal-description">Savor the flavor of the Restaurant Landing Page – a feast for the eyes and the taste buds. Tailored for culinary establishments, this landing page showcases your restaurant's ambiance, menu offerings, and special events. Entice visitors with mouthwatering visuals and an easy-to-navigate design that mirrors the hospitality of your establishment.</p>
+            <div class="technology-wrapper d-flex flex-wrap">
+                <div class="technology-item">HTML</div>
+                <div class="technology-item">CSS 3</div>
+                <div class="technology-item">Java Script</div>
+                <div class="technology-item">PHP</div>
+                <div class="technology-item">Laravel</div>
+                <div class="technology-item">Swiper</div>
+            </div>
+            <div class="button-group d-flex">
+                <a href="#" class="button-modal">
+                    See Github
+                </a>
+                <a href="#" class="button-modal">
+                    See Preview
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
