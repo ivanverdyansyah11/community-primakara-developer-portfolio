2a9f4c1cb3c37d84b16c3cdd890f8eefd395cf11
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-01 23:46:14 +0800

    Progress: Update Template Main

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -11,12 +11,14 @@
     <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
     {{-- END STYLE CSS --}}
 </head>
-<body>
+<body class="mb-5 pb-5">
     
     @include('components.navbar')
     @include('components.topbar')
 
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus, excepturi reprehenderit suscipit est fugit eum asperiores dignissimos eius vitae. Debitis dicta quas fuga eius autem fugiat totam, rem ducimus delectus numquam! Ex asperiores debitis enim quis esse laboriosam mollitia unde vel animi, dolorum nemo repellat voluptate alias, nisi dolorem repellendus at, accusantium tempora id quidem nam doloribus delectus? Unde delectus earum laborum facere beatae. Similique ipsum enim perspiciatis ipsam, debitis aperiam? Accusantium ut quam voluptatem non neque accusamus voluptate veniam deserunt doloribus vitae voluptatibus aut facere, unde id. Officiis placeat amet sed facilis minus laboriosam dolore repudiandae iusto porro harum et, hic, deserunt ea doloremque ipsum deleniti commodi beatae velit, quo obcaecati facere iure delectus. Officiis numquam incidunt ex error suscipit expedita accusamus exercitationem! Veritatis repellat tempore ratione, laudantium eos deserunt fugiat voluptatem optio nobis ut sapiente odio sint eum sunt. Beatae earum dolore doloremque necessitatibus deleniti et facilis nulla aspernatur. Reprehenderit ad ipsum molestias accusantium officiis quis aliquid voluptates voluptatum! Quia necessitatibus perspiciatis beatae quaerat magni reiciendis maiores consectetur, provident ullam fuga libero, similique iste modi natus fugit ipsum voluptas molestias molestiae sed totam. Sunt porro sit illo laudantium perspiciatis! Architecto error dicta quam perspiciatis non itaque voluptas minima!</p>
+    <main class="container">
+        @yield('content-page')
+    </main>
 
     {{-- SCRIPT JS --}}
     <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
