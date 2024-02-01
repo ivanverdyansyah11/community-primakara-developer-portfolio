532d65c130dea63d6dda74ca2721e915ba25ff2a
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-01 23:47:12 +0800

    Progress: Finish & Responsive Section Hero

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,20 @@
 @extends('layout.main')
 
-@section('content')
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus dolorem fugit at itaque numquam suscipit voluptas ipsum deserunt inventore perferendis!</p>
+@section('content-page')
+    <section class="hero text-lg-center">
+        <div class="row justify-content-lg-center">
+            <div class="col-xxl-10 d-flex flex-column align-items-lg-center">
+                <h1 class="headline">Empowering Developers, Building Futures Welcome to Primakara Developer Community</h1>
+                <p class="paragraph">Join a thriving ecosystem of passionate developers at Primakara Developer Community. Explore cutting-edge technologies, collaborate on innovative projects, and unlock a world of opportunities. Together, we code the future.</p>
+                <div class="button-group d-flex">
+                    <a href="#" class="button-primary">
+                        <span>Explore Website Now</span>
+                    </a>
+                    <a href="#" class="button-reverse">
+                        <span>Discover Our Projects</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
