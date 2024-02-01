0caa2ba4c4c691ac921fd5def353a9b35158f545
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-02 04:13:43 +0800

    Progress: Finish Modal Section Benefit

---
+++ resources/views/modals/benefit.blade.php
@@ -0,0 +1,95 @@
+<div class="modal fade" id="benefitOneModal" tabindex="-1" aria-labelledby="benefitOneModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content content-benefit">
+            <div class="header-wrapper d-flex align-items-center justify-content-between">
+                <h4 class="modal-headline">Collaborative Learning</h4>
+                <button type="button" data-bs-dismiss="modal" style="all: unset; cursor: pointer;">
+                    <img src="{{ asset('assets/images/icons/exit.svg') }}" alt="Exit Modal Icon" class="img-fluid" width="18">
+                </button>
+            </div>
+            <p class="modal-description">Engage in collaborative projects and group activities, fostering an environment where knowledge is shared freely, and members learn from each other's experiences.</p>
+            <div class="button-group d-flex">
+                <button type="button" class="button-modal" data-bs-toggle="modal" data-bs-target="#benefitFourModal">
+                    <img src="{{ asset('assets/images/icons/arrow-light.svg') }}" alt="Arrow Light Icon" class="img-fluid" width="14" height="6">
+                    Community Support
+                </button>
+                <button type="button" class="button-modal" data-bs-toggle="modal" data-bs-target="#benefitTwoModal">
+                    Exclusive Access
+                    <img src="{{ asset('assets/images/icons/arrow-light.svg') }}" alt="Arrow Light Icon" class="img-fluid" width="14" height="6">
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="benefitTwoModal" tabindex="-1" aria-labelledby="benefitTwoModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content content-benefit">
+            <div class="header-wrapper d-flex align-items-center justify-content-between">
+                <h4 class="modal-headline">Exclusive Access to Resources</h4>
+                <button type="button" data-bs-dismiss="modal" style="all: unset; cursor: pointer;">
+                    <img src="{{ asset('assets/images/icons/exit.svg') }}" alt="Exit Modal Icon" class="img-fluid" width="18">
+                </button>
+            </div>
+            <p class="modal-description">Gain access to a wealth of exclusive resources, including workshops, tutorials, and cutting-edge materials, designed to empower you with the latest skills and insights in the fast-paced world of technology.</p>
+            <div class="button-group d-flex">
+                <button type="button" class="button-modal" data-bs-toggle="modal" data-bs-target="#benefitOneModal">
+                    <img src="{{ asset('assets/images/icons/arrow-light.svg') }}" alt="Arrow Light Icon" class="img-fluid" width="14" height="6">
+                    Collaborative
+                </button>
+                <button type="button" class="button-modal" data-bs-toggle="modal" data-bs-target="#benefitThreeModal">
+                    Networking
+                    <img src="{{ asset('assets/images/icons/arrow-light.svg') }}" alt="Arrow Light Icon" class="img-fluid" width="14" height="6">
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="benefitThreeModal" tabindex="-1" aria-labelledby="benefitThreeModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content content-benefit">
+            <div class="header-wrapper d-flex align-items-center justify-content-between">
+                <h4 class="modal-headline">Networking Opportunities</h4>
+                <button type="button" data-bs-dismiss="modal" style="all: unset; cursor: pointer;">
+                    <img src="{{ asset('assets/images/icons/exit.svg') }}" alt="Exit Modal Icon" class="img-fluid" width="18">
+                </button>
+            </div>
+            <p class="modal-description">Connect with a diverse network of industry professionals, mentors, and fellow developers. Build meaningful relationships that can open doors to new opportunities, collaborations, and career advancements.</p>
+            <div class="button-group d-flex">
+                <button type="button" class="button-modal" data-bs-toggle="modal" data-bs-target="#benefitTwoModal">
+                    <img src="{{ asset('assets/images/icons/arrow-light.svg') }}" alt="Arrow Light Icon" class="img-fluid" width="14" height="6">
+                    Exclusive Access
+                </button>
+                <button type="button" class="button-modal" data-bs-toggle="modal" data-bs-target="#benefitFourModal">
+                    Community Support
+                    <img src="{{ asset('assets/images/icons/arrow-light.svg') }}" alt="Arrow Light Icon" class="img-fluid" width="14" height="6">
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="benefitFourModal" tabindex="-1" aria-labelledby="benefitFourModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content content-benefit">
+            <div class="header-wrapper d-flex align-items-center justify-content-between">
+                <h4 class="modal-headline">Community Support</h4>
+                <button type="button" data-bs-dismiss="modal" style="all: unset; cursor: pointer;">
+                    <img src="{{ asset('assets/images/icons/exit.svg') }}" alt="Exit Modal Icon" class="img-fluid" width="18">
+                </button>
+            </div>
+            <p class="modal-description">Experience the power of community support. Seek advice, share challenges, and receive mentorship from experienced developers within the Primakara community. Accelerate your learning and career growth with the guidance of seasoned professionals.</p>
+            <div class="button-group d-flex">
+                <button type="button" class="button-modal" data-bs-toggle="modal" data-bs-target="#benefitThreeModal">
+                    <img src="{{ asset('assets/images/icons/arrow-light.svg') }}" alt="Arrow Light Icon" class="img-fluid" width="14" height="6">
+                    Networking
+                </button>
+                <button type="button" class="button-modal" data-bs-toggle="modal" data-bs-target="#benefitOneModal">
+                    Collaborative
+                    <img src="{{ asset('assets/images/icons/arrow-light.svg') }}" alt="Arrow Light Icon" class="img-fluid" width="14" height="6">
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
