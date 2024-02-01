188387faaa1aee7623837cca1c30c30a0a017ce4
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-01 19:53:38 +0800

    Progress: Add & Update Portfolio Page

---
+++ resources/views/portfolio.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.main')
+
+@section('content')
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus dolorem fugit at itaque numquam suscipit voluptas ipsum deserunt inventore perferendis!</p>
+@endsection
