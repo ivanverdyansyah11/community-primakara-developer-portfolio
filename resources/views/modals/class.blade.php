8ed25ffd6e6902fd3b7f8fb42319f4de20993b64
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-02 01:06:05 +0800

    Progress: Finish Styling Modal Classes

---
+++ resources/views/modals/class.blade.php
@@ -0,0 +1,137 @@
+<div class="modal fade" id="designModal" tabindex="-1" aria-labelledby="designModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content content-class">
+            <div class="header-wrapper d-flex align-items-center justify-content-between">
+                <h4 class="modal-headline">UI/ UX Design</h4>
+                <button type="button" data-bs-dismiss="modal" style="all: unset; cursor: pointer;">
+                    <img src="{{ asset('assets/images/icons/exit.svg') }}" alt="Exit Modal Icon" class="img-fluid" width="18">
+                </button>
+            </div>
+            <p class="modal-description">Immerse yourself in the world of User Interface (UI) and User Experience (UX) design with Primakara Developer Community. Our UI/UX Design class is tailored for aspiring designers and developers who aspire to create visually stunning and user-centric digital experiences.</p>
+            <div class="checklist-wrapper d-flex flex-column">
+                <div class="checklist-item d-flex align-items-center">
+                    <img src="{{ asset('assets/images/icons/checklist.svg') }}" alt="Checklist Modal Icon" class="img-fluid" width="24px">
+                    <p class="checklist-value">Fundamentals of UI/UX</p>
+                </div>
+                <div class="checklist-item d-flex align-items-center">
+                    <img src="{{ asset('assets/images/icons/checklist.svg') }}" alt="Checklist Modal Icon" class="img-fluid" width="24px">
+                    <p class="checklist-value">User Research and Persona Development</p>
+                </div>
+                <div class="checklist-item d-flex align-items-center">
+                    <img src="{{ asset('assets/images/icons/checklist.svg') }}" alt="Checklist Modal Icon" class="img-fluid" width="24px">
+                    <p class="checklist-value">Wireframing and Prototyping</p>
+                </div>
+                <div class="checklist-item d-flex align-items-center">
+                    <img src="{{ asset('assets/images/icons/checklist.svg') }}" alt="Checklist Modal Icon" class="img-fluid" width="24px">
+                    <p class="checklist-value">Visual Design Techniques</p>
+                </div>
+                <div class="checklist-item d-flex align-items-center">
+                    <img src="{{ asset('assets/images/icons/checklist.svg') }}" alt="Checklist Modal Icon" class="img-fluid" width="24px">
+                    <p class="checklist-value">Interaction Design</p>
+                </div>
+            </div>
+            <div class="button-group d-flex">
+                <button type="button" class="button-modal" data-bs-toggle="modal" data-bs-target="#backModal">
+                    <img src="{{ asset('assets/images/icons/arrow-light.svg') }}" alt="Arrow Light Icon" class="img-fluid" width="14" height="6">
+                    Back End Development
+                </button>
+                <button type="button" class="button-modal" data-bs-toggle="modal" data-bs-target="#frontModal">
+                    Front End Development
+                    <img src="{{ asset('assets/images/icons/arrow-light.svg') }}" alt="Arrow Light Icon" class="img-fluid" width="14" height="6">
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="frontModal" tabindex="-1" aria-labelledby="frontModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content content-class">
+            <div class="header-wrapper d-flex align-items-center justify-content-between">
+                <h4 class="modal-headline">Front End Development</h4>
+                <button type="button" data-bs-dismiss="modal" style="all: unset; cursor: pointer;">
+                    <img src="{{ asset('assets/images/icons/exit.svg') }}" alt="Exit Modal Icon" class="img-fluid" width="18">
+                </button>
+            </div>
+            <p class="modal-description">Embark on a journey into the realm of Front End Development with Primakara Developer Community. Our comprehensive Front End Development class is designed to empower aspiring developers with the skills and knowledge needed to create captivating user interfaces and seamless user experiences.</p>
+            <div class="checklist-wrapper d-flex flex-column">
+                <div class="checklist-item d-flex align-items-center">
+                    <img src="{{ asset('assets/images/icons/checklist.svg') }}" alt="Checklist Modal Icon" class="img-fluid" width="24px">
+                    <p class="checklist-value">HTML5 and CSS3 Mastery</p>
+                </div>
+                <div class="checklist-item d-flex align-items-center">
+                    <img src="{{ asset('assets/images/icons/checklist.svg') }}" alt="Checklist Modal Icon" class="img-fluid" width="24px">
+                    <p class="checklist-value">Responsive Design Techniques</p>
+                </div>
+                <div class="checklist-item d-flex align-items-center">
+                    <img src="{{ asset('assets/images/icons/checklist.svg') }}" alt="Checklist Modal Icon" class="img-fluid" width="24px">
+                    <p class="checklist-value">JavaScript Essentials</p>
+                </div>
+                <div class="checklist-item d-flex align-items-center">
+                    <img src="{{ asset('assets/images/icons/checklist.svg') }}" alt="Checklist Modal Icon" class="img-fluid" width="24px">
+                    <p class="checklist-value">Front End Frameworks</p>
+                </div>
+                <div class="checklist-item d-flex align-items-center">
+                    <img src="{{ asset('assets/images/icons/checklist.svg') }}" alt="Checklist Modal Icon" class="img-fluid" width="24px">
+                    <p class="checklist-value">User Interface (UI) Design Principles</p>
+                </div>
+            </div>
+            <div class="button-group d-flex">
+                <button type="button" class="button-modal" data-bs-toggle="modal" data-bs-target="#designModal">
+                    <img src="{{ asset('assets/images/icons/arrow-light.svg') }}" alt="Arrow Light Icon" class="img-fluid" width="14" height="6">
+                    UI/ UX Design
+                </button>
+                <button type="button" class="button-modal" data-bs-toggle="modal" data-bs-target="#backModal">
+                    Back End Development
+                    <img src="{{ asset('assets/images/icons/arrow-light.svg') }}" alt="Arrow Light Icon" class="img-fluid" width="14" height="6">
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="backModal" tabindex="-1" aria-labelledby="backModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content content-class">
+            <div class="header-wrapper d-flex align-items-center justify-content-between">
+                <h4 class="modal-headline">Back End Development</h4>
+                <button type="button" data-bs-dismiss="modal" style="all: unset; cursor: pointer;">
+                    <img src="{{ asset('assets/images/icons/exit.svg') }}" alt="Exit Modal Icon" class="img-fluid" width="18">
+                </button>
+            </div>
+            <p class="modal-description">Dive into the core of web development with Primakara Developer Community's Back End Developer class. Designed for those aspiring to master the server-side of applications, this class equips you with the skills needed to architect robust, scalable, and efficient back-end systems.</p>
+            <div class="checklist-wrapper d-flex flex-column">
+                <div class="checklist-item d-flex align-items-center">
+                    <img src="{{ asset('assets/images/icons/checklist.svg') }}" alt="Checklist Modal Icon" class="img-fluid" width="24px">
+                    <p class="checklist-value">Server-Side Programming Languages</p>
+                </div>
+                <div class="checklist-item d-flex align-items-center">
+                    <img src="{{ asset('assets/images/icons/checklist.svg') }}" alt="Checklist Modal Icon" class="img-fluid" width="24px">
+                    <p class="checklist-value">Database Management</p>
+                </div>
+                <div class="checklist-item d-flex align-items-center">
+                    <img src="{{ asset('assets/images/icons/checklist.svg') }}" alt="Checklist Modal Icon" class="img-fluid" width="24px">
+                    <p class="checklist-value">API Development</p>
+                </div>
+                <div class="checklist-item d-flex align-items-center">
+                    <img src="{{ asset('assets/images/icons/checklist.svg') }}" alt="Checklist Modal Icon" class="img-fluid" width="24px">
+                    <p class="checklist-value">Authentication and Authorization</p>
+                </div>
+                <div class="checklist-item d-flex align-items-center">
+                    <img src="{{ asset('assets/images/icons/checklist.svg') }}" alt="Checklist Modal Icon" class="img-fluid" width="24px">
+                    <p class="checklist-value">Performance Optimization</p>
+                </div>
+            </div>
+            <div class="button-group d-flex">
+                <button type="button" class="button-modal" data-bs-toggle="modal" data-bs-target="#frontModal">
+                    <img src="{{ asset('assets/images/icons/arrow-light.svg') }}" alt="Arrow Light Icon" class="img-fluid" width="14" height="6">
+                    Front End Development
+                </button>
+                <button type="button" class="button-modal" data-bs-toggle="modal" data-bs-target="#designModal">
+                    UI/ UX Design
+                    <img src="{{ asset('assets/images/icons/arrow-light.svg') }}" alt="Arrow Light Icon" class="img-fluid" width="14" height="6">
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
