eb92bc00b311430371a34c67ce0797f7443742e3
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-02 22:57:19 +0800

    Progress: Revision Style Section CTA

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -513,18 +513,17 @@
 
     {{-- SECTION CTA --}}
     <section class="cta section-gap position-relative">
-        <div class="style-one"></div>
-        <div class="style-two"></div>
+        <div class="style"></div>
         <div class="row justify-content-center position-relative">
             <div class="col-11 col-lg-10 text-lg-center d-flex flex-column align-items-lg-center justify-content-lg-center">
                 <h1 class="headline">Unlock Your Potential with Primakara Developer Community</h1>
                 <p class="paragraph">Join a thriving community of passionate developers at Primakara. Whether you're a seasoned coder or just starting your journey, our community offers a dynamic space to collaborate, learn, and innovate.</p>
                 <div class="button-group d-flex">
                     <a href="#" class="button-primary">
-                        <span>Join the Community</span>
+                        <span>Join <div class="d-none d-md-inline-block">the</div> Community</span>
                     </a>
                     <a href="#" class="button-reverse">
-                        <span>Explore Projects</span>
+                        <span>Explore Our Projects</span>
                     </a>
                 </div>
             </div>
