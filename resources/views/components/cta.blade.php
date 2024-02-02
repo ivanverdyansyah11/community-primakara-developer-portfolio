953a883e57035a18e701356d4f295a4587f87883
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-03 07:37:14 +0800

    Progress: Update Link Redirect Section CTA

--- resources/views/components/cta.blade.php
+++ resources/views/components/cta.blade.php
@@ -6,10 +6,10 @@
             <h1 class="headline">Unlock Your Potential with Primakara Developer Community</h1>
             <p class="paragraph">Join a thriving community of passionate developers at Primakara. Whether you're a seasoned coder or just starting your journey, our community offers a dynamic space to collaborate, learn, and innovate.</p>
             <div class="button-group d-flex">
-                <a href="#" class="button-primary">
+                <a href="https://wa.me/6282147382985" class="button-primary" target="_blank">
                     <span>Join <div class="d-none d-md-inline-block">the</div> Community</span>
                 </a>
-                <a href="#" class="button-reverse">
+                <a href="{{ Route::is('mainpage') ? route('portfolio') : '#portfolio' }}" class="button-reverse">
                     <span>Explore Our Projects</span>
                 </a>
             </div>
