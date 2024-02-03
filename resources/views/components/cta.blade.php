ac1ef3e5bf21e8ce06e26673b512cb76bb6459cc
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-03 18:16:15 +0800

    Progress: Add AOS Component CTA

--- resources/views/components/cta.blade.php
+++ resources/views/components/cta.blade.php
@@ -1,15 +1,15 @@
 {{-- SECTION CTA --}}
-<section class="cta section-gap position-relative" id="cta">
+<section data-aos="fade" data-aos-duration="800" class="cta section-gap position-relative" id="cta">
     <div class="style"></div>
     <div class="row justify-content-center position-relative">
         <div class="col-11 col-lg-10 text-lg-center d-flex flex-column align-items-lg-center justify-content-lg-center">
-            <h1 class="headline">Unlock Your Potential with Primakara Developer Community</h1>
-            <p class="paragraph">Join a thriving community of passionate developers at Primakara. Whether you're a seasoned coder or just starting your journey, our community offers a dynamic space to collaborate, learn, and innovate.</p>
+            <h1 data-aos="fade-up" data-aos-duration="600" class="headline">Unlock Your Potential with Primakara Developer Community</h1>
+            <p data-aos="fade-up" data-aos-duration="600" class="paragraph">Join a thriving community of passionate developers at Primakara. Whether you're a seasoned coder or just starting your journey, our community offers a dynamic space to collaborate, learn, and innovate.</p>
             <div class="button-group d-flex">
-                <a href="https://wa.me/6282147382985" class="button-primary" target="_blank">
+                <a data-aos="fade-up" data-aos-duration="600" href="https://wa.me/6282147382985" class="button-primary" target="_blank">
                     <span>Join <div class="d-none d-md-inline-block">the</div> Community</span>
                 </a>
-                <a href="{{ Route::is('mainpage') ? route('portfolio') : '#home' }}" class="button-reverse">
+                <a data-aos="fade-up" data-aos-duration="600" data-aos-delay="150" href="{{ Route::is('mainpage') ? route('portfolio') : '#home' }}" class="button-reverse">
                     <span>Explore Our Projects</span>
                 </a>
             </div>
