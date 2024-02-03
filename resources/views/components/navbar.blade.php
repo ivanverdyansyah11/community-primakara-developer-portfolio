9867e9298676a77bcb99a8c5ed218f2a8e937238
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-03 16:47:59 +0800

    Progress: Revision Link Redirect Component Navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,23 +1,23 @@
 <nav class="navbar navbar-desktop navbar-expand-lg bg-body-tertiary">
     <div class="container">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="{{ Route::is('portfolio') ? route('mainpage') : '' }}">
             <img src="{{ asset('assets/images/brand/logo-brand.svg') }}" alt="Logo Brand" class="img-fluid" height="40">
         </a>
         <div class="collapse navbar-collapse d-none d-lg-flex justify-content-center" id="navbarNavAltMarkup">
             <div class="navbar-nav d-flex">
-                <a class="nav-link active" href="{{ Route::is('mainpage') ? '#home' : route('mainpage') }}">Home</a>
-                <a class="nav-link" href="{{ route('mainpage') }}/benefit">Benefit Community</a>
-                <a class="nav-link" href="{{ route('mainpage') }}/about">About Us</a>
-                <a class="nav-link" href="{{ route('portfolio') }}">Portfolio</a>
+                <a class="nav-link {{ Route::is('mainpage') ? 'active' : '' }}" href="{{ Route::is('portfolio') ? route('mainpage') : '' }}">Home</a>
+                <a class="nav-link" href="{{ Route::is('mainpage') ? '#benefit' : route('mainpage') . '/#benefit' }}">Benefit Community</a>
+                <a class="nav-link" href="{{ Route::is('mainpage') ? '#about' : route('mainpage') . '/#about' }}">About Us</a>
+                <a class="nav-link {{ Route::is('portfolio') ? 'active' : '' }}" href="{{ Route::is('mainpage') ? route('portfolio') : route('portfolio') . '#home' }}">Portfolio</a>
             </div>
         </div>
         <div class="navbar-wrapper d-flex d-lg-none">
-            <a class="nav-link active" href="{{ Route::is('mainpage') ? '#home' : route('mainpage') }}">Home</a>
-            <a class="nav-link" href="{{ route('mainpage') }}/benefit">Benefit Community</a>
-            <a class="nav-link" href="{{ route('mainpage') }}/about">About Us</a>
-            <a class="nav-link" href="{{ route('portfolio') }}">Portfolio</a>
+            <a class="nav-link {{ Route::is('mainpage') ? 'active' : '' }}" href="{{ Route::is('portfolio') ? route('mainpage') : '' }}">Home</a>
+            <a class="nav-link" href="{{ Route::is('mainpage') ? '#benefit' : route('mainpage') . '/#benefit' }}">Benefit Community</a>
+            <a class="nav-link" href="{{ Route::is('mainpage') ? '#about' : route('mainpage') . '/#about' }}">About Us</a>
+            <a class="nav-link {{ Route::is('portfolio') ? 'active' : '' }}" href="{{ Route::is('mainpage') ? route('portfolio') : route('portfolio') . '#home' }}">Portfolio</a>
         </div>
-        <a href="#" class="button-primary-small d-none d-lg-flex align-items-center">
+        <a href="https://wa.me/6282147382985" class="button-primary-small d-none d-lg-flex align-items-center" target="_blank">
             <span>Join the Community</span>
             <div class="arrow-dark"></div>
         </a>
